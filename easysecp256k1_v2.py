3e4ca8d2b0015ba7e5c02951a7de7a736f282bde
Jackson <76890597+jacksonjost@users.noreply.github.com>
2023-08-16 23:31:08 -0400

    Update easysecp256k1_v2.py

--- easysecp256k1_v2.py
+++ easysecp256k1_v2.py
@@ -1,10 +1,38 @@
+# smaller number example of secp256k1-type curve v2.0 | Jackson Jost
+
 import matplotlib.pyplot as plt
+from sympy.ntheory import isprime
+
+private_keyint = 112
+
+private_key = hex(private_keyint)
 
 # Constants for the curve y^2 = x^3 + 7 mod p
-# points in the corners mean that your p is not sutible, or if you receive an error - see quadratic residue property in the finite field
 a = 0
 b = 7
-p = 599 #MAKE SURE THIS VALUE IS A SUITABLE PRIME - check plot to see point distribution and test this
+p = 7789 #MAKE SURE THIS VALUE IS A SUITABLE PRIME - check plot to see point distribution and test this
+
+# Ensure p is prime
+if not isprime(p):
+    raise ValueError("p must be prime")
+
+# Function to find a generator point
+def find_generator_point():
+    for x in range(p):
+        y2 = (x**3 + b) % p
+        y = pow(y2, (p + 1) // 2, p)
+        if (y * y) % p == y2:
+            point = {'x': x, 'y': y}
+            # Additional checks for point order and suitability coming in the future
+            return point
+    raise ValueError("No suitable generator point found")
+
+# Find the generator point
+g = find_generator_point()
+#g = {
+#    'x': 101,
+#    'y': 3,
+#}
 
 # Modular Inverse
 def modinv(a, m=p):
@@ -16,100 +44,72 @@ def modinv(a, m=p):
         a, m = m % a, a
     return y
 
-def plot_curve(curve):
+# Double
+def double(point):
+    slope = ((3 * point['x'] ** 2) * modinv(2 * point['y'])) % p
+    x = (slope ** 2 - (2 * point['x'])) % p
+    y = (slope * (point['x'] - x) - point['y']) % p
+    return {'x': x, 'y': y}
+
+# Add
+def add(point1, point2):
+    if point1 == point2:
+        return double(point1)
+    slope = ((point1['y'] - point2['y']) * modinv(point1['x'] - point2['x'])) % p
+    x = (slope ** 2 - point1['x'] - point2['x']) % p
+    y = ((slope * (point1['x'] - x)) - point1['y']) % p
+    return {'x': x, 'y': y}
+
+# Multiply
+def multiply(k, point=g):
+    current = point
+    binary = bin(k)[2:]
+    for char in binary[1:]:
+        current = double(current)
+        if char == "1":
+            current = add(current, point)
+    return current
+
+# Coord
+def extract_coordinates(public_key):
+    if public_key.startswith('04'):
+        x_hex = public_key[2:66]
+        y_hex = public_key[66:]
+        x = int(x_hex, 16)
+        y = int(y_hex, 16)
+        return x, y
+    else:
+        raise ValueError("The provided public key does not appear to be uncompressed")
+
+def plot_curve():
     x_values = []
     y_values = []
-    for x in range(curve.p):
-        for y_sq in [(x**3 + curve.a * x + curve.b) % curve.p]:
-            y = pow(y_sq, (curve.p + 1) // 4, curve.p)
-            if (y * y - y_sq) % curve.p == 0:
-                for y_val in {y, curve.p - y}:
-                    x_values.append(x)
-                    y_values.append(y_val)
-
-    plt.scatter(x_values, y_values, s=1)
-    plt.title('Elliptic Curve Field')
+    for x in range(p):
+        y2 = (x**3 + b) % p
+        for y in range(p):
+            if (y**2) % p == y2:
+                x_values.append(x)
+                y_values.append(y)
+                break # We found one solution for y, no need to continue checking
+    plt.scatter(x_values, y_values, s=5)
     plt.xlabel('x')
     plt.ylabel('y')
+    plt.title('Elliptic Curve')
     plt.show()
 
-class EllipticCurve:
-    def __init__(self, a, b, p):
-        self.a = a
-        self.b = b
-        self.p = p
-        self.G, self.M = self.find_generator()
 
-    def find_generator(self):
-        max_order = 0
-        generator = None
-        for x in range(self.p):
-            for y_sq in [(x**3 + self.a * x + self.b) % self.p]:
-                # Check if y_sq is a quadratic residue
-                if pow(y_sq, (self.p - 1) // 2, self.p) != 1:
-                    continue
-                    
-                y = pow(y_sq, (self.p+1)//4, self.p)
-                # Debugging print statement
-                #print(f"x: {x}, y_sq: {y_sq}, y: {y}")
-                
-                if (y*y - y_sq) % self.p == 0:
-                    for y_val in {y, self.p-y}:
-                        point = {'x': x, 'y': y_val}
-                        order = 1
-                        current_point = point
-                        iteration = 0  # counter to avoid infinite loop
-                        while self.add(current_point, point) is not None and iteration < 100:
-                            current_point = self.add(current_point, point)
-                            order += 1
-                            iteration += 1
-                            # Debugging print statement
-                            #print(f"Current point: {current_point}, Point: {point}")
-                        
-                        if iteration == 100000:
-                            print("Reached max iterations, potential infinite loop")
-                            return None
-                        
-                        if order > max_order:
-                            max_order = order
-                            generator = point
-        return generator, max_order
+k = int(private_key, 16)
+point = multiply(k, g)
+x = format(point['x'], '064x')
+y = format(point['y'], '064x')
+public_key_uncompressed = "04" + x + y
+prefix = "02" if (point['y'] % 2 == 0) else "03"
+public_key_compressed = prefix + x
 
-    def double(self, point):
-        slope = ((3 * point['x'] ** 2 + a) * modinv(2 * point['y'])) % p
-        x = (slope ** 2 - 2 * point['x']) % p
-        y = (slope * (point['x'] - x) - point['y']) % p
-        return {'x': x, 'y': y}
+print(private_keyint)
+#print(public_key_uncompressed)
+x, y = extract_coordinates(public_key_uncompressed)
+print(f"Generator point: {g}")
+print(f"x: {x}\ny: {y}")
 
-    def add(self, point1, point2):
-        if point1 == point2:
-            return self.double(point1)
-        slope = ((point1['y'] - point2['y']) * modinv(point1['x'] - point2['x'])) % p
-        x = (slope ** 2 - point1['x'] - point2['x']) % p
-        y = ((slope * (point1['x'] - x)) - point1['y']) % p
-        return {'x': x, 'y': y}
-
-    def multiply(self, k, point):
-        current = point
-        binary = bin(k)[2:]
-        for char in binary[1:]:
-            current = self.double(current)
-            if char == "1":
-                current = self.add(current, point)
-        return current
-
-curve = EllipticCurve(a, b, p)
-private_key = 97
-G = curve.G # Set Generator Point
-max_order = curve.M
-public_key = curve.multiply(private_key, G)
-
-# Ensure the private key modulo the order of the subgroup
-if private_key <= max_order:
-    print(f"Private key: {private_key}")
-    print(f"Public key: {public_key}")
-    print(f"The generator is {G} with order {max_order}.")
-else:
-    print(f"This private key is outside of group order {max_order}")
-
-plot_curve(curve)
+plot_curve()
